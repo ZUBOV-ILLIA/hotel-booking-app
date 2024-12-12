2614649cd694a727d6576025a809877a1e377024
ZUBOV ILLIA <zubovillia@gmail.com>
2024-12-12 17:08:42 +0300

    added eslint, prettier, tailwind for prettier, basic setups

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { FaBed } from "react-icons/fa";
+
+function Header() {
+  return (
+    <div className="container m-2 mx-auto p-2 font-bold">
+      <FaBed size={24} className="text-yellow-400" />
+    </div>
+  );
+}
+
+export default Header;
